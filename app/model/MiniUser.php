c3711b8c017150967675d5f34f5ccadd20942be8
pp <826979076@qq.com>
2026-03-01 01:30:50 +0800

    优化返回数据 禁止泄露用户隐私

--- app/model/MiniUser.php
+++ app/model/MiniUser.php
@@ -7,9 +7,18 @@ use think\Model;
 
 /**
  * 小程序用户
+ * 接口返回时自动隐藏敏感字段（见 $hidden）
  */
 class MiniUser extends Model
 {
     protected $table = 'mini_user';
+
+    /** 接口返回 toArray() 时隐藏，避免泄露给前端 */
+    protected $hidden = [
+        'openid',
+        'unionid',
+        'mobile',
+        'last_login_ip',
+    ];
 }
 
